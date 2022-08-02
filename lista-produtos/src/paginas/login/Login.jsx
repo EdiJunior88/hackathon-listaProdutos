a82010542861e9140e5cfa2e7587595d96914ef6
EdiJunior88 <er.moura88@yahoo.com.br>
2022-08-02 11:35:33 -0300

    Atualizações da página LOGIN - Adição da biblioteca formik e yup (validação) - Criação de um template simples com email e cpf no CSS - atualização do index.html para o pt-br

--- lista-produtos/src/paginas/login/Login.jsx
+++ lista-produtos/src/paginas/login/Login.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import styles from './Login.module.css';
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      cpf: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   return (
-    <h1>Teste</h1>
-  )
-}
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor='email'>Email</label>
+      <input
+        id='email'
+        name='email'
+        type='email'
+        placeholder='Email'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <label htmlFor='cpf'>CPF</label>
+      <input
+        id='cpf'
+        name='cpf'
+        type='text'
+        placeholder='CPF'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <button type='submit'>Entrar</button>
+    </form>
+  );
+};
 
 export default Login;
