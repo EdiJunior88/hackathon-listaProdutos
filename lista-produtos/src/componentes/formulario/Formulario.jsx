ee0ae4f27fcb4b90b3ac703fa9cdacce6612d389
EdiJunior88 <er.moura88@yahoo.com.br>
2022-08-03 23:50:06 -0300

    Atualizações do componente Formulário - Adição do Formik e Yup
    Adição de variáveis de ambiente (.env) no projeto

--- lista-produtos/src/componentes/formulario/Formulario.jsx
+++ lista-produtos/src/componentes/formulario/Formulario.jsx
@@ -1,64 +1,90 @@
-import React, { useEffect, useRef, useState } from "react";
-import MD5 from "crypto-js/md5";
-import * as yup from "yup";
-import { useFormik } from "formik";
-import styles from "./Formulario.module.css";
+import React, { useEffect, useRef, useState } from 'react';
+import MD5 from 'crypto-js/md5';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import styles from './Formulario.module.css';
 
 const Formulario = () => {
-    const [email, setEmail] = useState("");
-    const [cpf, setCPF] = useState("");
+  const [email, setEmail] = useState('');
+  const [cpf, setCPF] = useState('');
 
-    /* Função para deixar o input CPF formatado com números */
-    const somenteNumeros = (evento) => {
-        const input = evento.target.value.replace(/\D/g, "");
-        setCPF(input);
-    };
+  /* Função para deixar o input CPF formatado com números */
+  const somenteNumeros = (evento) => {
+    const input = evento.target.value.replace(/\D/g, '');
+    setCPF(input);
+  };
 
-    /* Função que salva os dados dos inputs (Email e CPF) no localStorage */
-    /* e criptografa os inputs em hash MD5 para string */
-    function salvarDados() {
-        localStorage.setItem("criptografia", MD5(email + cpf).toString());
-    }
+  /* Função que salva os dados dos inputs (Email e CPF) no localStorage */
+  /* e criptografa os inputs em hash MD5 para string */
+  function salvarDados() {
+    localStorage.setItem('criptografia', MD5(email + cpf).toString());
+  }
 
-    return (
-        <div>
-            <form
-                onSubmit={(evento) => {
-                    evento.preventDefault();
-                }}
-            >
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    onChange={(evento) => setEmail(evento.target.value)}
-                />
+  const esquema = yup.object().shape({
+    email: yup.string().email().required('Preencha o email'),
+    cpf: yup.string().max(11).required('Preencha o CPF'),
+  });
 
-                <label htmlFor="cpf">CPF</label>
-                <input
-                    id="cpf"
-                    name="cpf"
-                    type="text"
-                    placeholder="CPF"
-                    maxLength={11}
-                    value={cpf}
-                    onChange={(evento) => {
-                        setCPF(evento.target.value);
-                        somenteNumeros(evento);
-                    }}
-                />
-                <button type="submit" onClick={salvarDados}>
-                    Entrar
-                </button>
-            </form>
+  const formik = useFormik({
+    validationSchema: esquema,
+    validateOnBlur: false,
+    validateOnChange: false,
+    initialValues: {
+      email: '',
+      cpf: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
-            <h3>
-                {email} - {cpf} - {MD5(email + cpf).toString()}
-            </h3>
-        </div>
-    );
+
+  return (
+    <div>
+      <form
+        onSubmit={(evento) => {
+          evento.preventDefault();
+          formik.handleSubmit();
+        }}>
+        <label htmlFor='email'>Email</label>
+        <input
+          id='email'
+          name='email'
+          type='email'
+          placeholder='Email'
+          onChange={(evento) => {
+            setEmail(evento.target.value);
+            formik.handleChange(evento);
+          }}
+        />
+        {formik.errors.email && (<div>{formik.errors.email}</div>)}
+
+        <label htmlFor='cpf'>CPF</label>
+        <input
+          id='cpf'
+          name='cpf'
+          type='text'
+          placeholder='CPF'
+          maxLength={11}
+          value={cpf}
+          onChange={(evento) => {
+            setCPF(evento.target.value);
+            somenteNumeros(evento);
+            formik.handleChange(evento);
+          }}
+        />
+        {formik.errors.cpf && (<div>{formik.errors.cpf}</div>)}
+
+        <button type='submit' onClick={salvarDados}>
+          Entrar
+        </button>
+      </form>
+
+      <h3>
+        {email} - {cpf} - {MD5(email + cpf).toString()}
+      </h3>
+    </div>
+  );
 };
 
 export default Formulario;
