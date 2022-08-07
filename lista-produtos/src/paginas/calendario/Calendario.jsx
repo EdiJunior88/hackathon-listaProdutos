e0eb5fbc82d9da2631e7662442216ba516c1764a
EdisonJuniorDesigner <mage1232010@gmail.com>
2022-08-07 14:11:24 -0300

    Fix

--- lista-produtos/src/paginas/calendario/Calendario.jsx
+++ lista-produtos/src/paginas/calendario/Calendario.jsx
@@ -8,64 +8,64 @@ import { ptBR } from "date-fns/locale";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const locales = {
-  "pt-BR": ptBR,
+    "pt-BR": ptBR,
 };
 
 const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
 });
 
 const events = [
-  {
-    title: "Big Meeting",
-    allDay: true,
-    start: new Date(2022, 7, 7),
-    end: new Date(2022, 7, 8),
-  },
-  {
-    title: "Vacation",
-    start: new Date(2022, 7, 7),
-    end: new Date(2022, 7, 8),
-  },
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2022, 7, 7),
+        end: new Date(2022, 7, 8),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2022, 7, 7),
+        end: new Date(2022, 7, 8),
+    },
 ];
 
 const Calendario = () => {
-  const [allEvents, setAllEvents] = useState(events);
+    const [allEvents, setAllEvents] = useState(events);
 
-  const { messages } = useMemo(
-    () => ({
-      messages: {
-        week: "Semana",
-        day: "Dia",
-        month: "Mês",
-        previous: "voltar",
-        next: "próxima",
-        today: "hoje",
-        agenda: "Lista Completa",
-      },
-    }),
-    []
-  );
+    const { messages } = useMemo(
+        () => ({
+            messages: {
+                week: "Semana",
+                day: "Dia",
+                month: "Mês",
+                previous: "voltar",
+                next: "próxima",
+                today: "hoje",
+                agenda: "Lista Completa",
+            },
+        }),
+        []
+    );
 
-  return (
-    <div className='App'>
-      <h1>Calendar</h1>
-      <Calendar
-        culture={"pt-BR"}
-        defaultView='week'
-        localizer={localizer}
-        events={allEvents}
-        startAccessor='start'
-        endAccessor='end'
-        messages={messages}
-        style={{ height: 500, margin: "50px" }}
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="mt-5">Lista Completa</h1>
+            <Calendar
+                culture={"pt-BR"}
+                defaultView="week"
+                localizer={localizer}
+                events={allEvents}
+                startAccessor="start"
+                endAccessor="end"
+                messages={messages}
+                style={{ height: 500, margin: "50px" }}
+            />
+        </div>
+    );
 };
 
 export default Calendario;
