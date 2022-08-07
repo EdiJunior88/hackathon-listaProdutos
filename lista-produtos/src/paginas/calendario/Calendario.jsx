fa1d806221a19494ff92ca56a44c0dc5b3592a9a
EdiJunior88 <er.moura88@yahoo.com.br>
2022-08-07 13:00:53 -0300

    Atualizações da página Calendário - Adição da biblioteca "react-big-calendar" - Deixando o calendário em PT-BR

--- lista-produtos/src/paginas/calendario/Calendario.jsx
+++ lista-produtos/src/paginas/calendario/Calendario.jsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useMemo, useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { ptBR } from "date-fns/locale";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const locales = {
+  "pt-BR": ptBR,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Big Meeting",
+    allDay: true,
+    start: new Date(2022, 7, 7),
+    end: new Date(2022, 7, 8),
+  },
+  {
+    title: "Vacation",
+    start: new Date(2022, 7, 7),
+    end: new Date(2022, 7, 8),
+  },
+];
 
 const Calendario = () => {
-  return(
-    <h1>Página Calendário</h1>
-  )
+  const [allEvents, setAllEvents] = useState(events);
+
+  const { messages } = useMemo(
+    () => ({
+      messages: {
+        week: "Semana",
+        day: "Dia",
+        month: "Mês",
+        previous: "voltar",
+        next: "próxima",
+        today: "hoje",
+        agenda: "Lista Completa",
+      },
+    }),
+    []
+  );
+
+  return (
+    <div className='App'>
+      <h1>Calendar</h1>
+      <Calendar
+        culture={"pt-BR"}
+        defaultView='week'
+        localizer={localizer}
+        events={allEvents}
+        startAccessor='start'
+        endAccessor='end'
+        messages={messages}
+        style={{ height: 500, margin: "50px" }}
+      />
+    </div>
+  );
 };
 
 export default Calendario;
